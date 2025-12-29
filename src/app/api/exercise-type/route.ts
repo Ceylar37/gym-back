b795fe066871930d0185820f4c88b1f3711c6c2a
ceylar37 <ceylar37@gmail.com>
2025-12-29 23:30:10 +0300

    fix: test cors

--- src/app/api/exercise-type/route.ts
+++ src/app/api/exercise-type/route.ts
@@ -2,11 +2,11 @@ import exerciseType from "@/modules/exercise-type";
 
 import { NextResponse } from "next/server";
 
+export const GET = exerciseType.controller.read;
+export const POST = exerciseType.controller.create;
+export const PATCH = exerciseType.controller.update;
 export const OPTIONS = async () => {
   return new NextResponse("", {
     status: 200,
   });
 };
-export const GET = exerciseType.controller.read;
-export const POST = exerciseType.controller.create;
-export const PATCH = exerciseType.controller.update;
