8594bbe3090e042ecae512b2b66a303639640a40
ceylar37 <ceylar37@gmail.com>
2025-12-29 23:21:17 +0300

    fix: options for exercise-type

--- src/app/api/exercise-type/route.ts
+++ src/app/api/exercise-type/route.ts
@@ -1,5 +1,8 @@
 import exerciseType from "@/modules/exercise-type";
 
+import { NextResponse } from "next/server";
+
+export const OPTIONS = () => new NextResponse();
 export const GET = exerciseType.controller.read;
 export const POST = exerciseType.controller.create;
 export const PATCH = exerciseType.controller.update;
